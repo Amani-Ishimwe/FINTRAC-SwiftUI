c5305db6bd0b1493d0a37b05b08cd97a4933c74f
Amani-Ishimwe <amaniishimwe36@gmail.com>
2025-10-22 18:47:28 +0200

    Initial Commit

---
+++ FINTRAC/FINTRACApp.swift
@@ -0,0 +1,17 @@
+//
+//  FINTRACApp.swift
+//  FINTRAC
+//
+//  Created by Amani on 10/22/25.
+//
+
+import SwiftUI
+
+@main
+struct FINTRACApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
